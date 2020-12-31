a05420e2d2ca061e846c437c3dfd4b96b25f9977
Sammi Song <songyunhui2008@gmail.com>
2020-12-31 14:35:31 -0800

    Remove not needed Configuration class

--- api/src/main/java/com/tuware/msbuild/MsbuildApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package com.tuware.msbuild;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.tuware.msbuild")
-public class MsbuildApplicationConfig {
-}
