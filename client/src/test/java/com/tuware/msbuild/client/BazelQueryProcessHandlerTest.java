5e0fa64bbf39bc1f84d266a239b8cdc22d1c4c33
Sammi Song <songyunhui2008@gmail.com>
2021-01-24 15:06:50 -0800

    parse command proto output by java Process inputstream.

--- client/src/test/java/com/tuware/msbuild/client/BazelQueryProcessHandlerTest.java
+++ client/src/test/java/com/tuware/msbuild/client/BazelQueryProcessHandlerTest.java
@@ -1,30 +1,30 @@
 package com.tuware.msbuild.client;
 
-import com.zaxxer.nuprocess.NuProcess;
-import com.zaxxer.nuprocess.NuProcessBuilder;
+import com.google.devtools.build.lib.query2.proto.proto2api.Build;
 import org.junit.jupiter.api.Test;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.concurrent.TimeUnit;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BazelQueryProcessHandlerTest {
     @Test
-    void bazel_output_as_xml() throws InterruptedException {
-        NuProcessBuilder nuProcessBuilder = new NuProcessBuilder(Arrays.asList(
+    void bazel_output_as_proto() throws IOException {
+        List<String> commands = Arrays.asList(
                 "cmd.exe",
                 "/c",
                 "bazel",
                 "query",
                 "...",
-                "--output=xml"));
-        Path bazelProjectRootPath = Paths.get("C:\\Users\\songy\\source\\repos\\tuware");
-        nuProcessBuilder.setCwd(bazelProjectRootPath);
-        BazelQueryProcessHandler bazelQueryProcessHandler = new BazelQueryProcessHandler();
-        nuProcessBuilder.setProcessListener(bazelQueryProcessHandler);
-        NuProcess nuProcess = nuProcessBuilder.start();
-        nuProcess.wantWrite();
-        nuProcess.waitFor(0, TimeUnit.SECONDS);
+                "--output=proto");
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.directory(new File("C:\\Users\\songy\\source\\repos\\tuware"));
+        processBuilder.command(commands);
+        Process process = processBuilder.start();
+        Build.QueryResult queryResult = Build.QueryResult.parseFrom(process.getInputStream());
+        assertEquals(38, queryResult.getTargetCount());
     }
 }
