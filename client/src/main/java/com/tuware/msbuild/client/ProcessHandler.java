83ff05d17fcf5c7a6d2d51a526ee8f2e209ac54f
Sammi Song <songyunhui2008@gmail.com>
2021-01-24 12:27:54 -0800

    Use NuProcessorBuild to read command output.

---
+++ client/src/main/java/com/tuware/msbuild/client/ProcessHandler.java
@@ -0,0 +1,18 @@
+package com.tuware.msbuild.client;
+
+import com.zaxxer.nuprocess.NuAbstractProcessHandler;
+
+import java.nio.ByteBuffer;
+
+public class ProcessHandler extends NuAbstractProcessHandler {
+
+    @Override
+    public void onStdout(ByteBuffer buffer, boolean closed) {
+        if (!closed) {
+            byte[] bytes = new byte[buffer.remaining()];
+            buffer.get(bytes);
+            System.out.print(new String(bytes));
+        }
+    }
+
+}
--- client/src/test/java/com/tuware/msbuild/client/BazelQueryApplicationTest.java
+++ client/src/test/java/com/tuware/msbuild/client/BazelQueryApplicationTest.java
@@ -1,49 +1,30 @@
 package com.tuware.msbuild.client;
 
+import com.zaxxer.nuprocess.NuProcess;
+import com.zaxxer.nuprocess.NuProcessBuilder;
 import org.junit.jupiter.api.Test;
-import org.springframework.util.Assert;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.concurrent.Executors;
-import java.util.function.Consumer;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class BazelQueryApplicationTest {
-
-    private static final boolean IS_WINDOWS = System.getProperty("os.name").toLowerCase().startsWith("windows");
-    private static final String homeDirectory = System.getProperty("user.home");
-    private Consumer<String> consumer = Assert::notNull;
-
-    private static class StreamGobbler implements Runnable {
-        private InputStream inputStream;
-        private Consumer<String> consumer;
-
-        public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
-            this.inputStream = inputStream;
-            this.consumer = consumer;
-        }
-
-        @Override
-        public void run() {
-            new BufferedReader(new InputStreamReader(inputStream)).lines().forEach(consumer);
-        }
-    }
-
+class BazelQueryApplicationTest {
     @Test
-    void givenProcess_whenCreatingViaRuntime_shouldSucceed() throws InterruptedException, IOException {
-        Process process;
-        if (IS_WINDOWS) {
-            process = Runtime.getRuntime().exec(String.format("cmd.exe /c dir %s", homeDirectory));
-        } else {
-            process = Runtime.getRuntime().exec(String.format("sh -c ls %s", homeDirectory));
-        }
-        StreamGobbler streamGobbler = new StreamGobbler(process.getInputStream(), consumer);
-        Executors.newSingleThreadExecutor().submit(streamGobbler);
-        assertEquals(0, process.waitFor());
+    void bazel_output_as_xml() throws InterruptedException {
+        NuProcessBuilder nuProcessBuilder = new NuProcessBuilder(Arrays.asList(
+                "cmd.exe",
+                "/c",
+                "bazel",
+                "query",
+                "...",
+                "--output=xml"));
+        Path bazelProjectRootPath = Paths.get("C:\\Users\\songy\\source\\repos\\tuware");
+        nuProcessBuilder.setCwd(bazelProjectRootPath);
+        ProcessHandler processHandler = new ProcessHandler();
+        nuProcessBuilder.setProcessListener(processHandler);
+        NuProcess nuProcess = nuProcessBuilder.start();
+        nuProcess.wantWrite();
+        nuProcess.waitFor(0, TimeUnit.SECONDS);
     }
-
 }
