95e6a0fbfb11b0ebd0dfd8eadb9a32f8d5a42232
Sammi Song <songyunhui2008@gmail.com>
2021-01-31 01:45:21 -0800

    Build cpp project solution.

--- msbuild/src/main/java/com/tuware/msbuild/cppwinrt/MSBuild.java
+++ msbuild/src/main/java/com/tuware/msbuild/cppwinrt/MSBuild.java
@@ -1,8 +1,12 @@
 package com.tuware.msbuild.cppwinrt;
 
+import com.github.jknack.handlebars.Handlebars;
 import com.tuware.msbuild.domain.solution.*;
 import javafx.util.Pair;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -10,7 +14,28 @@ import java.util.Collections;
 import java.util.UUID;
 
 public class MSBuild {
-    public Solution buildConsoleApp(
+
+    public String buildSolution(String name,
+                                String location,
+                                Path solutionPath,
+                                UUID projectGuid,
+                                UUID solutionGuid,
+                                MsBuildEnvironment msBuildEnvironment) throws URISyntaxException, IOException {
+        Handlebars handlebars = new Handlebars();
+
+        Path path = Paths.get(getClass().getResource("/templates/solution.hbs").toURI());
+        String template = String.join("\n", Files.readAllLines(path));
+
+        return handlebars.prettyPrint(true).compileInline(template).apply(buildConsoleApp(
+                name,
+                location,
+                solutionPath,
+                projectGuid,
+                solutionGuid,
+                msBuildEnvironment));
+    }
+
+    private Solution buildConsoleApp(
             String name,
             String location,
             Path solutionPath,
@@ -23,7 +48,7 @@ public class MSBuild {
                 .formatVersion(msBuildEnvironment.getFormatVersion())
                 .visualStudioVersion(msBuildEnvironment.getVisualStudioVersion())
                 .minimumVisualStudioVersion(msBuildEnvironment.getMinimumVisualStudioVersion())
-                .projectList(Arrays.asList(Project.builder()
+                .projectList(Collections.singletonList(Project.builder()
                         .projectTypeGuid(ProjectTypeGuid.CPP)
                         .name(name)
                         .location(Paths.get(location))
