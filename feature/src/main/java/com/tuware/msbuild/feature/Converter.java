802385830448754dddc8e6fd3f49cd611119a423
Sammi Song <songyunhui2008@gmail.com>
2021-02-22 00:07:53 -0800

    Remove FunctionInterface for converter interface.

--- feature/src/main/java/com/tuware/msbuild/feature/Converter.java
+++ feature/src/main/java/com/tuware/msbuild/feature/Converter.java
@@ -1,6 +1,5 @@
 package com.tuware.msbuild.feature;
 
-@FunctionalInterface
 public interface Converter {
     void convert(String msbuildProjectName, String bazelProjectPath, String msbuildProjectPath) throws ConverterException;
 }
