57791923a4e8ca7b57af11651fbd899d921c0942
Sammi Song <songyunhui2008@gmail.com>
2021-02-21 19:04:55 -0800

    Remove action query, focus on package query as the first step.

--- adapter/src/test/java/com/tuware/msbuild/adapter/mapper/CppProjectPackageMapperTest.java
+++ adapter/src/test/java/com/tuware/msbuild/adapter/mapper/CppProjectPackageMapperTest.java
@@ -1,8 +1,6 @@
 package com.tuware.msbuild.adapter.mapper;
 
-import com.google.devtools.build.lib.analysis.AnalysisProtosV2;
 import com.google.devtools.build.lib.query2.proto.proto2api.Build;
-import com.tuware.msbuild.adapter.query.ActionQueryAdapter;
 import com.tuware.msbuild.adapter.query.BazelWindowsProcessBuilder;
 import com.tuware.msbuild.adapter.query.PackageQueryAdapter;
 import com.tuware.msbuild.contract.adapter.AdapterException;
@@ -17,20 +15,16 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class CcBinaryPackageQueryAdapterTest {
+class CppProjectPackageMapperTest {
 
     private BazelQueryAdapter<Build.QueryResult> packageQuery;
-    private BazelQueryAdapter<AnalysisProtosV2.CqueryResult> actionQuery;
     private CppProjectPackageMapper ccBinaryPackageQueryAdapter;
-    private CppProjectActionMapper ccBinaryActionQueryMapper;
 
     @BeforeEach
     void setup() {
         BazelWindowsProcessBuilder bazelWindowsProcessBuilder = new BazelWindowsProcessBuilder();
         this.packageQuery = new PackageQueryAdapter(bazelWindowsProcessBuilder);
-        this.actionQuery = new ActionQueryAdapter(bazelWindowsProcessBuilder);
         this.ccBinaryPackageQueryAdapter = new CppProjectPackageMapper();
-        this.ccBinaryActionQueryMapper = new CppProjectActionMapper();
     }
 
     @Test
@@ -41,11 +35,4 @@ class CcBinaryPackageQueryAdapterTest {
         assertEquals(1, sourceFiles.size());
     }
 
-    @Test
-    void cquery() throws IOException, AdapterException {
-        File file = new ClassPathResource("stage3").getFile();
-        AnalysisProtosV2.CqueryResult cqueryResult = actionQuery.query(file.getAbsolutePath(), "...");
-        List<String> sourceFiles = ccBinaryActionQueryMapper.getSourceFileList(cqueryResult);
-        assertEquals(3, sourceFiles.size());
-    }
 }
