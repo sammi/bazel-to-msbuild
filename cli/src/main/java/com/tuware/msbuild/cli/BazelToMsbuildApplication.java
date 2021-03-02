b995463a12d908bdd70d37454e06cfa8069a7334
Sammi Song <songyunhui2008@gmail.com>
2021-03-01 18:53:52 -0800

    Add HelloWorldCommand to kick off cli command implementation.

--- cli/src/main/java/com/tuware/msbuild/cli/BazelToMsbuildApplication.java
+++ cli/src/main/java/com/tuware/msbuild/cli/BazelToMsbuildApplication.java
@@ -1,22 +1,29 @@
 package com.tuware.msbuild.cli;
 
+import com.tuware.msbuild.cli.command.HelloWorldCommand;
 import com.tuware.msbuild.feature.CppProjectFeature;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import picocli.CommandLine;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
     "com.tuware.msbuild.contract",
     "com.tuware.msbuild.feature",
     "com.tuware.msbuild.adapter",
+    "com.tuware.msbuild.cli.command",
 })
 public class BazelToMsbuildApplication implements CommandLineRunner {
 
-    @Autowired
     private CppProjectFeature cppProjectFeature;
+    private HelloWorldCommand helloWorldCommand;
+
+    public BazelToMsbuildApplication(CppProjectFeature cppProjectFeature, HelloWorldCommand helloWorldCommand) {
+        this.cppProjectFeature = cppProjectFeature;
+        this.helloWorldCommand = helloWorldCommand;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BazelToMsbuildApplication.class, args);
@@ -25,6 +32,8 @@ public class BazelToMsbuildApplication implements CommandLineRunner {
     @Override
     public void run(String... args) {
         assert(cppProjectFeature != null);
+        CommandLine commandLine = new CommandLine(helloWorldCommand);
+        commandLine.execute(args);
     }
 
 }
