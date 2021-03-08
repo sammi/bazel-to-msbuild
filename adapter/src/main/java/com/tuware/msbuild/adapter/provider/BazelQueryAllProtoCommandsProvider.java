923949c97816d43bcc7fe11079632f8b982873ca
Sammi Song <songyunhui2008@gmail.com>
2021-03-07 16:24:55 -0800

    adding cli wrap script.

--- adapter/src/main/java/com/tuware/msbuild/adapter/provider/BazelQueryAllProtoCommandsProvider.java
+++ adapter/src/main/java/com/tuware/msbuild/adapter/provider/BazelQueryAllProtoCommandsProvider.java
@@ -1,6 +1,7 @@
 package com.tuware.msbuild.adapter.provider;
 
 import com.tuware.msbuild.contract.adapter.Provider;
+import org.apache.commons.lang3.SystemUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -11,16 +12,25 @@ public class BazelQueryAllProtoCommandsProvider implements Provider<List<String>
 
     @Override
     public List<String> provide() {
-        //Using cmd /c to let windows find bazel from its PATH, otherwise needs provide bazel.cmd absolute path
         //--batch is required, otherwise the bazel process keep live after tests and holds bazel workspace file,
         //and it will block mvn clean command to remove target folder content.
-        return Arrays.asList(
-                "cmd",
-                "/c",
-                "bazel",
-                "--batch",
-                "query",
-                "...",
-                "--output=proto");
+        if(SystemUtils.IS_OS_WINDOWS) {
+            //Using cmd /c to let windows find bazel from its PATH, otherwise needs provide bazel.cmd absolute path
+            return Arrays.asList(
+                    "cmd",
+                    "/c",
+                    "bazel",
+                    "--batch",
+                    "query",
+                    "...",
+                    "--output=proto");
+        } else {
+            return Arrays.asList(
+                    "bazel",
+                    "--batch",
+                    "query",
+                    "...",
+                    "--output=proto");
+        }
     }
 }
---
+++ cli/b2m.cmd
@@ -0,0 +1,3 @@
+@echo OFF
+java -jar target\cli-0.1.0-SNAPSHOT.jar
+@echo ON
