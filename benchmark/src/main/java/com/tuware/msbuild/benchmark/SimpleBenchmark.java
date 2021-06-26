501197c705643e0170643373fd55bdb71c24f0f7
Sammi Song <songyunhui2008@gmail.com>
2021-06-26 00:15:36 -0700

    Add benchmark module.

---
+++ benchmark/src/main/java/com/tuware/msbuild/benchmark/SimpleBenchmark.java
@@ -0,0 +1,25 @@
+package com.tuware.msbuild.benchmark;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+public class SimpleBenchmark {
+
+    @Benchmark
+    public void wellHelloThere() {
+        // this method was intentionally left blank.
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(SimpleBenchmark.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+}
